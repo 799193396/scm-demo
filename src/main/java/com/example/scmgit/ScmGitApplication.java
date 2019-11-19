93415efae3f4db61724335b54493a72ab1e5e7ee
chenguomin <799193396@qq.com>
2019-11-19 13:53:51 +0800

    init scm-git

---
+++ src/main/java/com/example/scmgit/ScmGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.scmgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmGitApplication.class, args);
+	}
+
+}
